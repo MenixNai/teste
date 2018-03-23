3c956d88c833fc2c5fe7dd3bfc1ead9bccace835
Menix <menixnai@gmail.com>
2018-03-22 21:44:56 -0300

    Menix

---
+++ aula.py
@@ -0,0 +1,42 @@
+
+##Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+# a = int(input("Digite 1 numero:"))
+# if a%2==0:
+#     print ('È par')
+# else:
+#     print ('É impar')
+# a = int(input('Digite um numero: '))
+# b = int(input('Digite um numero: '))
+# if a>b:
+#     print(a,' é maior que ',b)
+# # else:
+# #     print(b,' é maior que ',a)
+# 3-Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+# a = int(input('Digite qual valor de A: '))
+# b = int(input('Digite qual valor de B: '))
+# c = int(input('Digite qual valor de C: '))
+# if (a+b)<c:
+#     print('A soma de ',a,' e ',b,' é menor que ',c )
+# else:
+# #         print('A soma de ',a,' e ',b,' é maior que ',c )
+# 4-Faça um Programa que verifique o sexo de uma pessoa. O usuário deve informar ‘F’ ou ‘M’ e o programa deve escrever “Feminino” ou “Masculino”, conforme a letra digitada.
+# a = str(input('Qual seu sexo escreva (M ou F): '))
+# if a==('f'):
+#     print('Feminino')
+# # else:
+# #     print('Masculino')
+# 5-Faça um Programa que peça um número inteiro e determine se ele é par ou ímpar. Dica: utilize o operador módulo (resto da divisão).
+# try:
+#     a = int(input('Digite um numero inteiro: '))
+#     if a%2==0:
+#         print ('È par')
+#     else:
+#         print ('É impar')
+# except:
+# #     print('O caractere digitado não é um numero inteiro')
+# 6-Faça um algoritmo que peça um número e se ele for par some 5, se não, some 8.
+a = int(input('Digite um numero inteiro: '))
+if a%2==0:
+    print (a+5)
+else:
+    print (a+8)
